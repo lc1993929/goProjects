4fcc5507a600cfc5282cb7283118b767146406fe
liuchang <736779328@qq.com>
2022-09-22 23:25:56 +0800

    chainlink demo

---
+++ src/ChainLinkDemo/contracts/PriceConsumerV3.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+contract PriceConsumerV3 {
+    AggregatorV3Interface public priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function getDecimals() public view returns (uint8) {
+        uint8 decimals = priceFeed.decimals();
+        return decimals;
+    }
+}
